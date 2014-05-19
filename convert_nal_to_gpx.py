3f1b8ff242c9645e9f4930ae75d62814ef85a4eb
benke <benke@x200s.(none)>
2014-05-19 22:36:03 +0530

    added binary NAL file for testing

--- convert_nal_to_gpx.py
+++ convert_nal_to_gpx.py
@@ -3,7 +3,7 @@
 import decode_nal
 
 def main():
-    nal_file = open('../20131203_05_42_31.NAL')
+    nal_file = open('20131203_05_42_31.NAL')
     nal_bin_data=nal_file.read()
     
     block_number = decode_nal.num_blocks(nal_bin_data)
